faa7f25e103d3a83685d05467d3df2c181593ac4
persuasive-animation-2602 <dorinatasevska@gmail.com>
2020-10-03 18:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function decrement (n){
 }
 
 function makeInt(string) {
-  return parseInt('string', 10)
+  return parseInt(string , 10)
 }
 function preserveDecimal(string){
   return parseFloat('string')
